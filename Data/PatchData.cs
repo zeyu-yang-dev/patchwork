5b4d52a1eb0536bc41b3656c342134cbf113bd9f
Zeyu Yang <123612557+zeyu-yang-dev@users.noreply.github.com>
2026-04-17 22:52:50 +0200

    feat: add Patch and PatchData structure

---
+++ Data/PatchData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Patchwork.Entity;
+
+namespace Patchwork.Data;
+
+public static class PatchData
+{
+    public static List<Patch> CreateAllPatches()
+    {
+        return
+        [
+            new Patch(
+                id: 0,
+                shape: new bool[,]
+                {
+                    { false, false, false, false, false },
+                    { false, false, false, false, false },
+                    { false, false, true,  true,  false },
+                    { false, false, false, false, false },
+                    { false, false, false, false, false }
+                },
+                moneyCost: 0,
+                timeCost: 0,
+                income: 0
+            ),
+
+            new Patch(
+                id: 1,
+                shape: new bool[,]
+                {
+                    { false, false, false, false, false },
+                    { false, false, true,  false, false },
+                    { false, false, true,  true,  true  },
+                    { false, false, true,  false, false },
+                    { false, false, false, false, false }
+                },
+                moneyCost: 0,
+                timeCost: 0,
+                income: 0
+            )
+        ];
+    }
+}
---
+++ Entity/Patch.cs
@@ -0,0 +1,14 @@
+﻿namespace Patchwork.Entity;
+
+public class Patch(int id, bool[,] shape, int moneyCost, int timeCost, int income)
+{
+    public int Id { get; } = id;
+
+    public bool[,] Shape { get; } = shape;
+
+    public int MoneyCost { get; } = moneyCost;
+
+    public int TimeCost { get; } = timeCost;
+
+    public int Income { get; } = income;
+}
