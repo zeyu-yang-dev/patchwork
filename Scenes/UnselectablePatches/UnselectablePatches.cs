72ef92453967a55cb22742c4a8d5f6a6f7982f37
Zeyu Yang <123612557+zeyu-yang-dev@users.noreply.github.com>
2026-05-04 22:48:42 +0200

    feat: implement scrolling of displayed patches

--- Scenes/UnselectablePatches/UnselectablePatches.cs
+++ Scenes/UnselectablePatches/UnselectablePatches.cs
@@ -14,12 +14,15 @@ public partial class UnselectablePatches : Control
 
 	// For scroll functionality.
 	private int _patchDisplayOffset = 0;
-	private List<TextureRect> _displayNodes = [];
+	private readonly List<TextureRect> _displayNodes = [];
 	
 	private RootService _rootService;
 	
 	public override void _Ready()
 	{
+		MouseFilter = MouseFilterEnum.Stop;
+		MouseExited += OnMouseExited;
+		
 		for (var indexInDisplay = 0; indexInDisplay < PatchDisplayAmount; indexInDisplay++)
 		{
 			var displayNode = new TextureRect();
@@ -30,11 +33,11 @@ public partial class UnselectablePatches : Control
 				0
 			);
 			displayNode.Texture = GetPatchTexture(indexInDisplay);
+			displayNode.MouseFilter = MouseFilterEnum.Ignore;
 			
 			_displayNodes.Add(displayNode);
 			AddChild(displayNode);
 		}
-		
 	}
 	
 	public void Initialize(RootService rootService)
@@ -45,11 +48,41 @@ public partial class UnselectablePatches : Control
 
 	public override void _GuiInput(InputEvent @event)
 	{
-		
+		if (@event is not InputEventMouseButton { Pressed: true } mouseButton)
+		{
+			return;
+		}
+
+		if (mouseButton.ButtonIndex is not MouseButton.WheelUp and not MouseButton.WheelDown)
+		{
+			return;
+		}
+
+		if (mouseButton.ButtonIndex == MouseButton.WheelUp)
+		{
+			_patchDisplayOffset = Mathf.Max(0, _patchDisplayOffset - 1);
+		}
+		else
+		{
+			_patchDisplayOffset += 1;
+		}
+
+		RefreshVisual();
+		AcceptEvent();
 	}
-
-
-
+	
+	/// <summary>
+	/// Resets _patchDisplayOffset when the cursor lefts this area.
+	/// </summary>
+	private void OnMouseExited()
+	{
+		_patchDisplayOffset = 0;
+		RefreshVisual();
+	}
+	
+	/// <summary>
+	/// Returns a resized patch texture.
+	/// </summary>
 	private static Texture2D GetPatchTexture(int id)
 	{
 		var texture = ResourceLoader.Load<Texture2D>($"{OriginalTextureDirectory}/{id}.png");
@@ -58,6 +91,9 @@ public partial class UnselectablePatches : Control
 		return ImageTexture.CreateFromImage(image);
 	}
 	
+	/// <summary>
+	/// Refreshes the displayed patches with relation to _patchDisplayOffset.
+	/// </summary>
 	private void RefreshVisual()
 	{
 		var unselectablePatches = _rootService.CurrentGame.PatchShop.GetUnselectablePatches();
