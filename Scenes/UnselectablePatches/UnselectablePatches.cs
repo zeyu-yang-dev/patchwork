f1af580fb0cf21e184235ea39f131552dd3c8002
Zeyu Yang <123612557+zeyu-yang-dev@users.noreply.github.com>
2026-05-04 21:52:33 +0200

    feat: implement initial display of unselectable patches

--- Scenes/UnselectablePatches/UnselectablePatches.cs
+++ Scenes/UnselectablePatches/UnselectablePatches.cs
@@ -6,23 +6,40 @@ namespace Patchwork.Scenes.UnselectablePatches;
 
 public partial class UnselectablePatches : Control
 {
-	private float _patchSize = 50.0f;
-	private int _patchNumber;
+	private const string OriginalTextureDirectory = "res://Assets/Patches/original";
+	private const float PatchSize = 50.0f;
+	private const float PatchSpacing = 1.0f;
+	private const int PatchDisplayAmount = 12;
 	
-	private int _patchOffset = 0;
+	private int _patchDisplayOffset = 0;
 	
 	private RootService _rootService;
 	
 	
 	public override void _Ready()
 	{
-		
+		for (var indexInDisplay = 0; indexInDisplay < PatchDisplayAmount; indexInDisplay++)
+		{
+			var displayNode = new TextureRect();
+
+			displayNode.Scale = new Vector2(
+				PatchSize / 250.0f, 
+				PatchSize / 250.0f
+			);
+			displayNode.Position = new Vector2(
+				indexInDisplay * (PatchSize + PatchSpacing),
+				0
+			);
+			displayNode.Texture = GetPatchTexture(indexInDisplay);
+			
+			AddChild(displayNode);
+		}
 		
 	}
 	
 	public void Initialize(RootService rootService)
 	{
-		_patchNumber = Mathf.FloorToInt(Size.X / _patchSize);
+		
 		
 		_rootService = rootService;
 		_rootService.StateChanged += OnGameStateChanged;
@@ -33,10 +50,23 @@ public partial class UnselectablePatches : Control
 		
 	}
 
+
+
+	private static Texture2D GetPatchTexture(int id)
+	{
+		return ResourceLoader.Load<Texture2D>($"{OriginalTextureDirectory}/{id}.png");
+	}
 	
 	
 	private void RefreshVisual()
 	{
+		var unselectablePatches = _rootService.CurrentGame.PatchShop.GetUnselectablePatches();
+
+		for (var indexInDisplay = 0; indexInDisplay < PatchDisplayAmount; indexInDisplay++)
+		{
+			var indexInShop = indexInDisplay + _patchDisplayOffset;
+			if (indexInShop >= unselectablePatches.Count) return;
+		}
 		
 	}
 	
